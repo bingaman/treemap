27b088c7fe20b4c9c9f1f89ffa14fff612d5db24
Michael Bingaman <bingaman@gmail.com>
2018-10-02 17:06:45 -0500

    debug js

--- js/chicago.js
+++ js/chicago.js
@@ -1,4 +1,5 @@
     mapboxgl.accessToken = PUBLIC_ACCESS_TOKEN;
+    var data = DATASETS_BASE + 'features?access_token=' + DATASETS_ACCESS_TOKEN;
 
     const map = new mapboxgl.Map({
       container: 'map',
@@ -17,57 +18,29 @@
 
     map.addControl(Draw, 'top-left');
 
-    var url = DATASETS_BASE + 'features?access_token=' + DATASETS_ACCESS_TOKEN;
     editorData=$.ajax({
         url: url
-    }).done(function(data){
+    }).done(function(data) {
       map.on('style.load', function (e) { 
-      console.log(url);
-      map.addSource('removed-trees', {
-        'type': 'geojson',
-        'data': data
-        /*'data': {
-          'type': 'FeatureCollection',
-          'features': []
-        }*/
-      });
-
-      map.addLayer({
-        'id': 'selected-trees',
-        'type': 'circle',
-        'source': 'removed-trees',
-        'interactive': true,
-        'paint': {
-          'circle-color': 'rgba(189,0,0,77)',
-          'circle-radius': 3
-        } 
-      }, 'removed-trees');
-
-    });
-
-    function getDataSet(startID) {
-
-      /*$.get(url, params, function (data) {
-        var features = {
+        console.log(url);
+        map.addSource('removed-trees', {
+          'type': 'geojson',
+          'data': data
+          /*'data': {
             'type': 'FeatureCollection',
             'features': []
-        };
-
-        data.features.forEach(function (feature) {
-          feature.properties.id = feature.id;
+          }*/
         });
 
-        features.features = data.features;
-
-        var lastFeatureID = data.features[data.features.length - 1].id;
-        getDataSet(lastFeatureID);
-
-        //console.log(features);
-
-        //TREES.setData(features);
-      });*/
-    }
-
-    function addSourcesAndLayers() {
-      //TREES = new mapboxgl.GeoJSONSource({});
+        map.addLayer({
+          'id': 'selected-trees',
+          'type': 'circle',
+          'source': 'removed-trees',
+          'interactive': true,
+          'paint': {
+            'circle-color': 'rgba(189,0,0,77)',
+            'circle-radius': 3
+          } 
+        }, 'removed-trees');
+      });
     }
